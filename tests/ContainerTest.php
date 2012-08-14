11eea7fc7702a6f8727c56986c5fedc1c19f16f5
Gonzalo Ayuso <gonzalo123@gmail.com>
2012-08-14 19:41:32 +0200

    Improved test for Container

---
+++ tests/ContainerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Gfw package.
+ *
+ * (c) Gonzalo Ayuso <gonzalo123@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Gfw\Container;
+use Gfw\Responser;
+use Gfw\Parser;
+use Gfw\View;
+use Gfw\Instancer;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ContainerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testContainer()
+    {
+        $request = Request::create('/foo/var.json', 'GET');
+        $container = new Container($request);
+        $this->assertTrue($container['request'] instanceof Request);
+    }
+
+    public function testContainer2()
+    {
+        $request = Request::create('/foo/var.json', 'GET');
+        $container = new Container($request);
+        $this->assertTrue($container['responser'] instanceof Responser);
+        $this->assertTrue($container['instancer'] instanceof Instancer);
+        $this->assertTrue($container['parser'] instanceof Parser);
+    }
+
+    public function testViewNotInitialized()
+    {
+        $request = Request::create('/foo/var.json', 'GET');
+        $container = new Container($request);
+        try {
+            $view = $container['view'];
+        } catch (\Exception $e) {
+            $this->assertEquals('Identifier "view" is not defined.', $e->getMessage());
+        }
+    }
+
+    public function testView()
+    {
+        $request = Request::create('/foo/var.json', 'GET');
+        $container = new Container($request);
+        $container->setUpViewEnvironment(__DIR__, TRUE);
+        $this->assertTrue($container['view'] instanceof View);
+    }
+}
