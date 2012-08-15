8398e459f55cfc7f1d0fe0f41de9273394db2db0
Gonzalo Ayuso <gonzalo123@gmail.com>
2012-08-15 18:11:28 +0200

    checking why tests fails in travis and not in my pc

--- tests/DiTest.php
+++ tests/DiTest.php
@@ -29,6 +29,7 @@ class DiTest extends \PHPUnit_Framework_TestCase
         $container['view']->registerNamespace('App', __DIR__ . '/templates');
 
         $instancer = new Instancer($container);
-        $this->assertEquals('Hi Gonzalo', 'Hi Gonzalo');
+        var_dump($instancer->invokeAction());
+        $this->assertEquals('Hi Gonzalo', $instancer->invokeAction());
     }
 }
